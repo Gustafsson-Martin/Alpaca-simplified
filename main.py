2b9e7dacc24a523577c1e8d3f54bc925b6d6f365
Gustafsson-Martin <martin.gustafsson.work@gmail.com>
2019-12-19 11:14:11 +0100

    testing data api

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import requests
 import json
+from datetime import datetime
 from api_keys import *
 
-TRADE_URL = "https://paper-api.alpaca.markets"
+TRADE_URL = "https://paper-api.alpaca.markets/v2"
 DATA_URL = "https://data.alpaca.markets/v1"
 
 headers = {
@@ -10,14 +11,38 @@ headers = {
     "APCA-API-SECRET-KEY" : API_SECRET_KEY
 }
 
-body = {
-    "symbol": "CIEN",
-    "qty": 10,
-    "side": "sell",
-    "type": "market",
-    "time_in_force": "day"
+symbols = ["CIEN", "WPX", "TSLA"]
+symbols_string = ""
+for symbol in symbols:
+    symbols_string += symbol + ","
+symbols_string = symbols_string[:-1]
+
+print(symbols_string)
+
+#body = {
+#    "symbol": "CIEN",
+#    "qty": 60,
+#    "side": "buy",
+#    "type": "market",
+#    "time_in_force": "day"
+#}
+
+params = {
+    "symbols": symbols_string,
+    "limit": 1000
 }
 
-response = requests.post(BASE_URL + "/v2/orders", data=json.dumps(body),headers=headers)
-#response = requests.get(BASE_URL + "/v2/account/configurations", data=json.dumps(body), headers=headers)
-print(json.loads(response.content))
+
+#response = requests.post(TRADE_URL + "/orders", data=json.dumps(body),headers=headers)
+response = requests.get("https://data.alpaca.markets/v1/bars/1Min", params=params, headers=headers)
+res = json.loads(response.content)
+first_time = res['CIEN'][0]['t']
+last_time = res['CIEN'][len(res['CIEN'])-1]['t']
+first_datetime = datetime.fromtimestamp(first_time)
+last_datetime = datetime.fromtimestamp(last_time)
+print(first_datetime.strftime("%Y-%m-%d %H:%M:%M"))
+print(last_datetime.strftime("%Y-%m-%d %H:%M:%M"))
+
+for r in res:
+    print(r)
+#print(json.loads(response.content))
