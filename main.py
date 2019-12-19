b1b53c0ba17dc1b177c64fea009da2eb33b5c9c2
Martin Gustafsson <margu424@student.liu.se>
2019-12-19 16:57:23 +0100

    buy, sell function, more testing for market api

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import json
 from datetime import datetime
 from api_keys import *
+from stock import Stock
 
 TRADE_URL = "https://paper-api.alpaca.markets/v2"
 DATA_URL = "https://data.alpaca.markets/v1"
@@ -11,28 +12,49 @@ headers = {
     "APCA-API-SECRET-KEY" : API_SECRET_KEY
 }
 
-symbols = ["CIEN", "WPX", "TSLA"]
-symbols_string = ""
-for symbol in symbols:
-    symbols_string += symbol + ","
-symbols_string = symbols_string[:-1]
-
+symbols = ['CIEN', 'TSLA', 'APPL']
+symbols_string = ','.join(symbols)
 print(symbols_string)
 
-#body = {
-#    "symbol": "CIEN",
-#    "qty": 60,
-#    "side": "buy",
-#    "type": "market",
-#    "time_in_force": "day"
-#}
 
 params = {
     "symbols": symbols_string,
     "limit": 1000
 }
 
+def buy(symbol, quantity, **kwargs):
+    body = {
+        "symbol": symbol,
+        "qty": quantity,
+        "side": "buy",
+        "type": kwargs.get("type", "market"),
+        "time_in_force": kwargs.get("time_in_force", "day")
+    }
+    response = requests.post(TRADE_URL + "/orders", data=json.dumps(body), headers=headers)
+    content = json.loads(response.content)
+    if (response.status_code // 100 == 2):
+        print(("Bought %d stocks of %s") % (quantity, symbol))
+    else:
+        print("Error: " + content["message"])
 
+
+def sell(symbol, quantity, **kwargs):
+    body = {
+        "symbol": symbol,
+        "qty": quantity,
+        "side": "sell",
+        "type": kwargs.get("type", "market"),
+        "time_in_force": kwargs.get("time_in_force", "day")
+    }
+    response = requests.post(TRADE_URL + "/orders", data=json.dumps(body), headers=headers)
+    content = json.loads(response.content)
+    if (response.status_code // 100 == 2):
+        print(("Sold %d stocks of %s") % (quantity, symbol))
+    else:
+        print("Error: " + content["message"])
+
+buy("CIEN", 10)
+exit()
 #response = requests.post(TRADE_URL + "/orders", data=json.dumps(body),headers=headers)
 response = requests.get("https://data.alpaca.markets/v1/bars/1Min", params=params, headers=headers)
 res = json.loads(response.content)
@@ -40,9 +62,16 @@ first_time = res['CIEN'][0]['t']
 last_time = res['CIEN'][len(res['CIEN'])-1]['t']
 first_datetime = datetime.fromtimestamp(first_time)
 last_datetime = datetime.fromtimestamp(last_time)
-print(first_datetime.strftime("%Y-%m-%d %H:%M:%M"))
-print(last_datetime.strftime("%Y-%m-%d %H:%M:%M"))
-
+print(first_datetime.strftime("%Y-%m-%d %H:%M:%S"))
+print(last_datetime.strftime("%Y-%m-%d %H:%M:%S"))
+print(res)
+stocks = []
 for r in res:
+    stocks.append(Stock(r, res[r]))
     print(r)
+
+print(stocks[1])
+
+#for price in stocks[0]:
+#    print(price.high)
 #print(json.loads(response.content))
